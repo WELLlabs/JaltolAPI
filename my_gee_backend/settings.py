211c4ed8a9fe54b3e4c0c847824ce94cb1e1654a
Craig Dsouza <craigds022@gmail.com>
2025-10-24 17:49:54 +0530

    securing gee service account key

--- my_gee_backend/settings.py
+++ my_gee_backend/settings.py
@@ -22,7 +22,8 @@ from datetime import timedelta
 load_dotenv()
 
 # Set the path to the credentials file
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './creds/credentials'
+# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './creds/credentials'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.getenv('GOOGLE_APPLICATION_CREDENTIALS', './creds/ee-service-account.json')
 
 GOOGLE_EARTH_ENGINE_API_KEY = os.getenv('GOOGLE_EARTH_ENGINE_API_KEY')
 GOOGLE_OAUTH2_CLIENT_ID = os.getenv('GOOGLE_OAUTH2_CLIENT_ID')
